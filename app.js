1b9de8ea29c92161d313c328b0899236dd95e8fc
Green081081 <blessededet94@gmail.com>
2023-04-28 09:07:10 +0100

    Created a post, put, and delete request

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.use(parser.json())
-app.use('/', require('./routes'))
+  .use(parser.urlencoded({ extended: false }))
+  .use(parser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+  })
+  .use('/', require('./routes'))
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err)
