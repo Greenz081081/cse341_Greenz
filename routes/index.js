322f92dd8b95cf5c7dd3644021fbf6ceb9f40fe5
Green081081 <blessededet94@gmail.com>
2023-05-01 16:34:44 +0100

    Get, Post, Put, Delete, with swagger UI

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@
 const express = require('express');
 // eslint-disable-next-line semi
 const router = express.Router();
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json')
+// const swaggerUi = require('swagger-ui-express')
+// const swaggerFile = require('./swagger-output.json')
 
 // eslint-disable-next-line semi
 router.use('/contacts', require('./contacts'));
-router.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+// router.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+router.use('/', require('./swagger'))
 
 module.exports = router
